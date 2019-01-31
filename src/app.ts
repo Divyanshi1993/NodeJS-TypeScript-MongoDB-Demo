cd3830c4856d11dff4ae399b2db0429f6f0b3758
divyanshi1993 <divyanshi.parashar1994@gmail.com>
2019-01-31 02:17:53 +0200

    Heroku deployment error fixed.

--- src/app.ts
+++ src/app.ts
@@ -4,26 +4,32 @@ import { Routes } from "./routes/crmRoutes";
 import * as mongoose from "mongoose";
 
 class App {
-    public app : express.Application=express();
-    public routePrv: Routes = new Routes();
-    //public mongoUrl: string = 'mongodb://localhost:27017/CRMdb';
-    public mongoUrl : string = process.env.MONGODB_URI;  
 
-    constructor(){
-    this.config();    
-    this.mongoSetup();
-    this.routePrv.routes(this.app); 
+    public app: express.Application = express();
+    public routePrv: Routes = new Routes();
+    public mongoUrl : string = process.env.MONGODB_URI;
+    //public mongoUrl: string = 'mongodb://localhost:27017/CRMdb';
+
+    constructor() {
+        this.config();
+        this.mongoSetup();
+        this.routePrv.routes(this.app);     
+    }
+
+    private config(): void{
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+        // serving static files 
+        this.app.use(express.static('public'));
     }
 
     private mongoSetup(): void{
         (<any>mongoose).Promise = global.Promise;
-        mongoose.connect(this.mongoUrl,{useNewUrlParser: true});    
-    }
-
-    private config():void{
-        this.app.use(bodyParser.json);
-        this.app.use(bodyParser.urlencoded({extended:false}));
+        mongoose.connect(this.mongoUrl, {useNewUrlParser: true});    
+        
+    
     }
 
 }
+
 export default new App().app;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import app from './app';
-const PORT = 3500;
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log('Express server listening on port ' + PORT);
+app.listen(process.env.PORT, () => {
+    console.log('Express server listening on port ' + process.env.PORT);
 })
